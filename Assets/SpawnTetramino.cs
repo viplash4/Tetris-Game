c7eec0ace690ae1da1754d6b48aec72bf3ca6f9f
Max Zalubovskiy <44732505+viplash4@users.noreply.github.com>
2020-02-17 20:50:14 +0200

    Initial commit

---
+++ Assets/SpawnTetramino.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnTetramino : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject[] Tetraminoes;
+    void Start()
+    {
+        NewTetramino();
+    }
+
+    // Update is called once per frame
+    public void NewTetramino()
+    {
+        Instantiate(Tetraminoes[Random.Range(0, Tetraminoes.Length)], transform.position, Quaternion.identity);
+    }
+}
